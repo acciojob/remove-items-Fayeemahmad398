ddd0ed36f952c91ae7ac6a0a1640a16a86b878a6
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-05-29 14:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.querySelector("input").onclick=function(){
+	
+	let selectEle=document.getElementById("colorSelect");
+let color=selectEle.value;
+let childrens=selectEle.children;
+for(let i=0;i<childrens.length;i++)
+	{
+		if(childrens[i].textContent===color){
+			selectEle.children[i].remove();
+		}
+	}
+
+}
+	
